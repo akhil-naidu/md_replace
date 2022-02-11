f05ae773cffbb8cefc205486e206c26b67884bb1
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-11 18:10:18 +0530

    update: v0.0.3

--- replace.js
+++ replace.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const eejs = require('md_mudoc/node/eejs/');
-
-
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = args.content + "<link href='../static/plugins/md_replace/static/css/fontello.css' rel='stylesheet'>";
   return cb();
